0e34be8d78fef5f0893ff100d3f02a96d6f1b608
liangcheng11521 <17cliang1@stu.edu.cn>
2019-11-23 00:19:33 +0800

    problemListDemo

--- BackEnd/Django_v1/GL/course/urls.py
+++ BackEnd/Django_v1/GL/course/urls.py
@@ -1,9 +1,10 @@
 # 引入path
 from django.urls import path
-from . import views
+from .views import SubmitProblemList
 # 正在部署的应用的名称
 app_name = 'course'
 
 urlpatterns = [
     path('course/', views.course, name='course'),
+    url(r'^problemList/$', SubmitProblemList.as_view(), name="SubmitProblemList"),
 ]
--- BackEnd/Django_v1/GL/course/views.py
+++ BackEnd/Django_v1/GL/course/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+import Question
 # 导入 HttpResponse 模块
 from django.http import HttpResponse
 
 # 视图函数
 def course(request):
     return HttpResponse("Hello World!")
+
+class SubmitProblemList(View):
+    """提交题目列表"""
+
+    def get(self, request):
+        questions = Question.objects.filter(submit_user=request.user)
+    return HttpResponse(questions)
