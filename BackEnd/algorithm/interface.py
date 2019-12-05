4c183e0c4758ed2f9766b2cc395882babc3e0b30
illumina7e <illumina7e@outlook.com>
2019-12-06 06:52:39 +0800

    update algo

---
+++ BackEnd/algorithm/interface.py
@@ -0,0 +1,27 @@
+def update_cap(user_id):
+    '''
+    更新一位被试者的能力值，练习时每做完一道题时调用。
+
+    Paramenters
+    -----------------
+        user_id: str, 被试者的ID（唯一标识符）
+    '''
+    pass
+    
+    return
+
+def recommend_que(user_id):
+    '''
+    推荐一道最符合能力值的试题，被试者请求下一题时调用。
+
+    Paramenters
+    -----------------
+        user_id: str, 被试者的ID（唯一标识符）
+
+    return
+    -----------------
+        match_que_id: str, 最符合能力值试题的题号
+    '''
+    pass
+
+    return match_que_id
